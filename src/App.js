94004d827cb3dbe61ced2ad51f297b6ba90b351d
Lu Nerbutti <lunerbutti@gmail.com>
2022-10-02 16:35:46 -0300

    react s first steps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// function miFuncon () {
+//   return null;
+// }
+// const miFuncionFlecha = () =>{
+//   return null;
+// }
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <header>
+  <h1 className ="rojo">header</h1>
+  </header>
+  </>
   );
 }
 
-export default App;
+export default App
+
+// {/* <p> Hola Mundo </p>
+
+// const nodoPadre = document.getElementById("padre")
+// const p = document.createElement("p")
+// p.innerHTML = "Hola Mundo" */}
