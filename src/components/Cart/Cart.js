f354c23359f6f8018995a681dc5c1029651b2ea9
Lu Nerbutti <lunerbutti@gmail.com>
2022-11-10 20:27:12 -0300

    trying netlify

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, {useContext} from "react";
+import { CartContext, Context } from "../Context/CartContesxt";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-   return(
-     <h3>Cart view</h3>)
+  const {CartContext} = useContext (Context);
+  if (CartContext.length === 0) {
+  return (
+     <h3>There is not product in the cart, may click <link to="/">HERE</link>
+     </h3>);
 }
-
+return (
+  <>
+  {CartContext.map((listaProducto) => (
+    <h2 key={listaProducto.id}>{listaProducto.nombre}</h2>
+  ) )}
+  </>
+);
+};
 export default Cart
